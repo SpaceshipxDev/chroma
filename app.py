d2c2701dfd6278e0856705d990d9b145afbcade4
SpaceshipxDev <150497663+SpaceshipxDev@users.noreply.github.com>
2023-12-27 07:43:27 +1100

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
-# Use an official Python runtime as a parent image
-FROM python:3.8-slim
+from flask import Flask
 
-# Set the working directory in the container
-WORKDIR /app
+app = Flask(__name__)
 
-# Copy the current directory contents into the container at /app
-COPY . /app
+@app.route('/')
+def home():
+    return 'baaa'
 
-# Install any needed packages specified in requirements.txt
-RUN pip install --no-cache-dir -r requirements.txt
-
-# Make port 5000 available to the world outside this container
-EXPOSE 5000
-
-# Define environment variable
-ENV NAME World
-
-# Run app.py when the container launches
-CMD ["python", "app.py"]
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
